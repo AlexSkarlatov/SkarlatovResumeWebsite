380e69b8a05b40a201f190feaf124bf213dcacb7
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-04-10 11:00:45 -0400

    update nav component using css bootstrap3
    I learned a little bigt more about how to use css bootstrap and applied that knowledge to make a fast and easy to create simple nav component which floats to the left of my resume web site making it look nice. Thus my quest to improve the layout of this page continues as i seek to master the various design flow options available in the angular library but i know that i should alsdo learn about css bootstrap and generic templates to put in to the css files of my components.

--- mock-skill.ts
+++ mock-skill.ts
@@ -1,6 +1,15 @@
 import { Skill } from './skill';
 
 export const SKILLS: Skill[] = [
-  {id: 1, name: 'cisco', description: 'how i got this skill is insert words here '},
-  
+  {id: 1, name: 'Cisco Router/Switch networking architecture and troubleshooting', description: 'how i got this skill is insert words here '},
+  { id: 2 , name: 'Angular' , description: ''},
+  { id: 3 , name: 'Java Application Development' , description: ''},
+  { id: 4, name: 'C' , description: ''},
+  { id: 5, name: 'SQL' , description: ''},
+  { id: 6, name: 'HTML5/CSS' , description: ''},
+  { id: 7, name: 'Ruby Scripting' , description: ''},
+  { id: 8, name: 'BASH Scripting on servers' , description: ''},
+  { id: 9, name: 'Splunk Development' , description: ''},
+  { id: 10, name: 'Informatica' , description: ''},
+  { id: 11, name: 'IBM Integration Bus' , description: ''}
 ];
